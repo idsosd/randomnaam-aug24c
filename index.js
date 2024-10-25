b56fc741bb14c1cb18963d99e7885bb1053ab54b
Ids Osinga <ip.osinga@alfa-college.nl>
2024-10-25 11:10:46 +0200

    v2024-10-22

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const namen=[
     "Ids",
     "Michiel",
     "Ismail",
-    "Janine"
+    "Janine",
+    "Junior" 
 ];
 //voorb: namen[0] = "Noah"; namen[7]="Taha"; namen[13]="Ids"; namen[14]="undefined"
 //als we dus een random getal tussen 0 en 13 genereren, kunnen we met dat getal
@@ -26,4 +27,15 @@ function randomname()
     let random_nmbr = Math.floor(Math.random() * namen.length);
     //alert("De gekozen naam is: " + namen[random_nmbr]);
     document.getElementById("naam").innerText = namen[random_nmbr];
+}
+
+function random2names()
+{
+    let random_nmbr = Math.floor(Math.random() * namen.length);
+    let random_nmbr2 = Math.floor(Math.random() * namen.length);
+    while(random_nmbr == random_nmbr2){
+        random_nmbr2 = Math.floor(Math.random() * namen.length);
+    }
+    //alert("De gekozen naam is: " + namen[random_nmbr]);
+    document.getElementById("naam").innerText = namen[random_nmbr] + " en " + namen[random_nmbr2];
 }
